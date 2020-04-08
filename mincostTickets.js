c34fcea4f4d708d285aa8b526d61903e443b9f67
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-04-08 12:27:31 -0700

    came up with a sound solution in the bottom, using 3 options dp approach

--- mincostTickets.js
+++ mincostTickets.js
@@ -4,25 +4,10 @@
  * @return {number}
  */
 const mincostTickets = (days, costs) => {
-    /*
-    By using dynamic programming solution, we have 2 adjustments
-    1: any day <= 0 will cost 0
-    2: When we cannto find the cost
-    for that day, pick the cloest available cost before that day.
+    
+    
 
-    For example: days = [0, 1, 4, 6, 7, 8, 20] && costs = [2, 7, 15]
-    for day 20:
-        1 day pass will be 2 + dp[19]
-    7 days pass will be 7 + dp[13]
-    30 days pass will be 15
-    Since we do not need the ticket
-    for
-    day 19;
-    the closest one we need to have a ticket will be day 8
-    so
-    if we buy one day pass, the cost will be 2 + dp[19] >> 2 + dp[8]
-    Same logic can be applied to 7 day pass with 7 + dp[13] >> 7 + dp[8]
-    */
+
 };
 
 
@@ -31,3 +16,28 @@ console.log(mincostTickets(days, costs), 11)
 
 days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31], costs = [2, 7, 15]
 console.log(mincostTickets(days, costs), 17)
+
+/*
+option #1 = cost 2 for 1 day
+option #2 = cost 7 for 7 day
+option #3 = cost 15 for 30 day
+
+check the furthest you can go back on option:
+
+[1,          2,                  3,                   4,                  5,      
+(2, 7, 15)  (2+dp[2-1], 7, 15)  (2+dp[3-1], 7, 15)   (2+dp[4-1], 7,15)   (2+[dp5-1], 7+dp[5-7], 15+dp[5-30]) <= minimum of these options
+2            2+dp[1] = 4         2+dp[2] = 6          2+dp[3] = 8        2+dp[4] = 9
+min = 2               4                   6                      7              7
+
+6,                        7,                           8,                          9,                     
+(2+dp[6-1], 7, 15)    (2+dp[7-1], 7+dp[7-7], 15)   (2+dp[8-1], 7+dp[8-7], 15)  (2+dp[9-1], 7+dp[9-7], 15)
+2+dp[5] = 9            2+dp[6] = 9                   2+dp[7]= 9, 7+dp[1]=9      2+dp[8] = 11, 7+dp[2]= 11
+min = 7                     7                           9                         11
+
+
+10,                               30,                                     31
+(2+dp[10-1], 7+dp[10-7], 15)     (2+dp[30-1], 7+dp[30-7], 15+dp[30-30])   (2+dp[31-1], 7+dp[31-7], 15+dp[31-30])
+2+dp[9] = 13, 7+dp[3] = 13        2+dp[29] = inf, 7+dp[23] = inf           2+dp[30] = 17, 7+dp[24]=inf, 15+dp[1] = 17
+min = 13                            min = 15                                17
+
+*/
