5118ea2ea4b848c01f8d83ad1faed0d064bbd5f0
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-04-08 20:00:40 -0700

    added sound solution using dynamic programming

--- mincostTickets.js
+++ mincostTickets.js
@@ -5,32 +5,42 @@
  */
 const mincostTickets = (days, costs) => {
 
-//     const minCost = []
-//     minCost[0] = 0
+    const dp = new Array(days[days.length-1]+1)
+    dp[0] = 0
 
-//     for (let i=0; i<days.length; i++) {
-//         let option1, option7, option30
+    for (let i=0; i<days.length; i++) {
+        let option1, option7, option30;
 
-//         option1 = costs[0] + highestPossibleMin(minCost, minCost[days[i-1]] )
-//         option7 = costs[1] + highestPossibleMin(minCost, minCost[days[i-7]] )
-//         option30 = costs[2] + highestPossibleMin(minCost, minCost[days[i-30]] )
-//         console.log(option1, option7, option30)
-//         minCost[days[i]] = Math.min(option1, option7, option30)
-//     }
+        option1 = costs[0]
+        option7 = costs[1]
+        option30 = costs[2]
 
-//     console.log(minCost)
-//     return minCost[days[days.length-1]]
-// };
 
-// const highestPossibleMin = (minCost, day) => {
-//     if (day <= 0) {
-//         return 0
-//     } else {
-//         while(minCost[day] === undefined) {
-//             day--
-//         }
-//         return minCost[day]
-//     }
+        if (days[i]-1 >= 0) {
+            option1+= findNextVal(dp, days[i]-1)
+        }
+
+        if (days[i]-7 >= 0) {
+            option7+= findNextVal(dp, days[i]-7)
+        }
+
+        if (days[i]-30 >= 0) {
+            option30+=findNextVal(dp, days[i]-30)
+        }
+
+        dp[days[i]] = Math.min(option1, option7, option30)
+
+    }
+
+    return dp[days[days.length-1]]
+
+}
+
+const findNextVal = (dp, day) => {
+    while (dp[day] === undefined) {
+        day--
+    }
+    return dp[day]
 }
 
 
@@ -47,8 +57,16 @@ option #2 = cost 7 for 7 day
 option #3 = cost 15 for 30 day
 
 check the furthest you can go back on option:
-     1                     4                             6                           7
-(2, 7, 15)    (2+dp[1], 7+dp[-3], 15+dp[-26])  (2+dp[4], 7+dp[-1], 15+dp[4])  (2+dp[6], 7+dp[0], 15+dp[6])
-    2             4        7        15             6        7       15           8       7        15
-    2                      4                            6
+
+days
+0   1           4                   6                   7                       8                    20
+                                  
+0   2           2+dp[1]=4           2+dp[4]=6           2+dp[6]=8               2+dp[7]=9            2+dp[8]=11
+0   7           7+dp[neg]=7         7+dp[neg]=7         7+dp[7-7]=7             7+dp[7-1]=9          7+dp[20-7]->[8]=16
+0   15          15+dp[neg]=15       15+dp[neg]=15       15+dp[neg]=15           15+dp[neg]=15        15+dp[neg]=15
+min 
+0   2           4                   6                   7                       9                    11
+
+finding the previous possible travel day after subtracting (1,7,30) is the dp value needs to be added.
+
 */
