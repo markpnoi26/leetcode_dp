6c39ceb1971b0e2d2b3a570a3bdcc3a6871eca6c
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-04-28 17:36:03 -0700

    added edit distance

---
+++ editDistance.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+const minDistance = (word1, word2) => {
+
+};
+
+let word1 = "horse", word2 = "ros"
+console.log(minDistance(word1, word2), 3)
+
+
+word1 = "intention", word2 = "execution"
+console.log(minDistance(word1, word2), 5)
